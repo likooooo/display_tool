1d4c56f1c65890ee27c9b45c479976f1375fe6c2
like <ke.li@yuweioptica.com>
2025-10-11 07:59:19 +0800

    add image_2d command

--- examples/image_2d.cpp
+++ examples/image_2d.cpp
@@ -1,7 +1,9 @@
 #include "glfw_window_2d.h"
+#include <string>
 
-int main() 
+int main(int argc, char** argv) 
 {
-    glfw_initializer().create2d(window_type::pipline).async_loop(30).event_loop();
+    window_type type = argc == 1 ? window_type::pipline : (window_type)(std::stoi(argv[1])); 
+    glfw_initializer().create2d(type).async_loop(30).event_loop();
     return 0;
 }
